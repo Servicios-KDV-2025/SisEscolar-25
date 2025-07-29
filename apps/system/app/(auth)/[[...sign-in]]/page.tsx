4f5bf9dae7aa3968d6a898b018a45efc0052e19d
belterrwan <ramirezibarra95@hotmail.com>
2025-07-29 14:21:55 -0600

    Enhance authentication page layout and sign-out functionality
    - Added a button wrapper around the SignOutButton for improved styling and user experience.
    - Updated the welcome message and added a description for the school listing feature.
    - Centered content vertically and horizontally for better visual alignment.

--- apps/system/app/(auth)/[[...sign-in]]/page.tsx
+++ apps/system/app/(auth)/[[...sign-in]]/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { SignIn, SignOutButton } from "@clerk/nextjs";
+import { Button } from "@repo/ui/components/shadcn/button";
 import CustomSignIn from "components/CustomSignIn";
 import { Authenticated, Unauthenticated } from "convex/react";
 
@@ -7,8 +8,13 @@ export default function authPage() {
   return (
     <div>
         <Authenticated>
+          <div className="flex flex-col items-center justify-center h-screen">
             <p>Bienvenido</p>
-            <SignOutButton />
+            <p>Aui se mostrara el listado de las escuelas, si no tienes uno no mostrara nada</p>
+            <Button asChild className="mt-4 bg-red-500 hover:bg-red-600 text-white rounded-full cursor-pointer">
+              <SignOutButton />
+            </Button>
+          </div>
         </Authenticated>
         <Unauthenticated>
           <div className="flex flex-col items-center justify-center h-screen">
