08a7cfb21dea30c698113006c17fc68315b7bca7
belterrwan <ramirezibarra95@hotmail.com>
2025-07-29 14:28:31 -0600

    Update authentication page to display user names in welcome message
    - Integrated useUser hook from Clerk to fetch user details.
    - Modified the welcome message to include the user's first and last name for a personalized experience.

--- apps/system/app/(auth)/[[...sign-in]]/page.tsx
+++ apps/system/app/(auth)/[[...sign-in]]/page.tsx
@@ -1,15 +1,16 @@
 'use client'
 import { SignIn, SignOutButton } from "@clerk/nextjs";
 import { Button } from "@repo/ui/components/shadcn/button";
-import CustomSignIn from "components/CustomSignIn";
 import { Authenticated, Unauthenticated } from "convex/react";
+import { useUser } from "@clerk/nextjs";
 
 export default function authPage() {
+  const { user } = useUser();
   return (
     <div>
         <Authenticated>
           <div className="flex flex-col items-center justify-center h-screen">
-            <p>Bienvenido</p>
+            <p>Bienvenido {user?.firstName} {user?.lastName}</p>
             <p>Aui se mostrara el listado de las escuelas, si no tienes uno no mostrara nada</p>
             <Button asChild className="mt-4 bg-red-500 hover:bg-red-600 text-white rounded-full cursor-pointer">
               <SignOutButton />
