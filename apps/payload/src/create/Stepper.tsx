b486839665d55af587388e17475b628764c80c6f
belterrwan <ramirezibarra95@hotmail.com>
2025-08-20 12:59:19 -0600

    Add Convex integration: update package dependencies, implement ConvexClientProvider in layout, and create school management functions in Convex. and add form for create schools

--- apps/payload/src/create/Stepper.tsx
+++ apps/payload/src/create/Stepper.tsx
@@ -6,6 +6,7 @@ import { School, User, CopySlash } from 'lucide-react'
 import { SignOutButton, useAuth } from '@clerk/nextjs'
 import { SignUp } from './SignUp'
 import { SignIn } from './SignIn'
+import SchoolForm from './SchoolForm'
 
 
 const { Stepper: StepperUi, useStepper } = defineStepper(
@@ -62,15 +63,15 @@ export const Stepper: React.FC = () => {
             </StepperUi.Navigation>
             {methods.switch({
               'step-1': (step) => <ClerkComponent />,
-              'step-2': (step) => <Content id={step.id} />,
+              'step-2': (step) => <SchoolForm />,
               'step-3': (step) => <Content id={step.id} />,
             })}
             <StepperUi.Controls>
-              {!methods.isLast && !methods.isFirst && (
+              {/* {!methods.isLast && !methods.isFirst && (
                 <Button variant="secondary" onClick={methods.prev} disabled={methods.isFirst}>
                   Previous
                 </Button>
-              )}
+              )} */}
 
               {!methods.isFirst && (
                 <Button onClick={methods.isLast ? () => {} : methods.next}>
