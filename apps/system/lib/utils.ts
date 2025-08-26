bb5d142baad9e94976073511e8e7ea7504e50b46
EmilioATC <torrescontreras2002@gmail.com>
2025-08-26 13:56:59 -0600

    Agregar funcionalidad de calendario escolar y gestión de eventos. Se incluye un nuevo componente para el calendario, diálogos para crear y editar eventos, y se implementan esquemas de validación para los eventos y tipos de eventos. También se añaden nuevas dependencias y utilidades para la gestión de clases CSS.

--- apps/system/lib/utils.ts
+++ apps/system/lib/utils.ts
@@ -1,2 +1,9 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
 export const rootDomain =
   process.env.NEXT_PUBLIC_ROOT_DOMAIN || 'localhost:3000';
+
+  export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+  }
---
+++ apps/system/schema/calendar.ts
@@ -0,0 +1,14 @@
+import { z } from "@repo/zod-config/index";
+
+export const CalendarSchema = z.object({
+    date: z.date().min(1, "La fecha es obligatoria"),
+    eventTypeId: z.string().min(1, "Debe seleccionar un tipo de evento"),
+    description: z
+        .string()
+        .max(500, "La descripción no puede exceder 500 caracteres")
+        .optional(),
+    schoolCycleId: z.string().min(1, "Debe seleccionar un ciclo escolar"),
+    status: z.enum(['active', 'inactive']).optional()
+});
+
+export type CalendarFormValues = z.infer<typeof CalendarSchema>;
