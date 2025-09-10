4fc7461cdaec6e0b9fe6cc64467a52404821951c
belterrwan <ramirezibarra95@hotmail.com>
2025-09-10 11:53:38 -0600

    payload delete

--- apps/payload/src/app/api/checkout/route.ts
+++
@@ -1,6 +0,0 @@
-import { stripeCheckout } from '@/endpoints/stripeCheckout'
-
-export const runtime = 'nodejs' 
-export async function POST(req: Request) {
-  return stripeCheckout(req)
-}
--- apps/payload/src/app/api/stripe/webhook/route.ts
+++
@@ -1,8 +0,0 @@
-import { stripeWebhook } from '@/endpoints/stripeWebhook' // o el path donde pusiste tu función
-
-export const runtime = 'nodejs'
-export const dynamic = 'force-dynamic'
-
-export async function POST(req: Request) {
-  return stripeWebhook(req)
-}
