8b74a3fa78091776d570a93a10a9bd813fd8f345
EmilioATC <torrescontreras2002@gmail.com>
2025-09-18 12:26:47 -0600

    delete merkating

--- apps/marketing/src/blocks/ImagenConTexto/Component.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import RichText from '@/components/RichText'
-
-export const ImagenConTextoComponent: React.FC = () => {
-  return null
-}
--- apps/marketing/src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250918_173222 from './20250918_173222';
-
-export const migrations = [
-  {
-    up: migration_20250918_173222.up,
-    down: migration_20250918_173222.down,
-    name: '20250918_173222'
-  },
-];
