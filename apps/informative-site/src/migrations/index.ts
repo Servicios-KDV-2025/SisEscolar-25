6e902d994d960463b8d57a4ba96c5ca7e787a53c
alanlopeztechio <alan.lopez@g-tech.io>
2025-09-18 12:40:56 -0600

    Subiendo nuevo proyecto

---
+++ apps/informative-site/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250918_184020 from './20250918_184020';
+
+export const migrations = [
+  {
+    up: migration_20250918_184020.up,
+    down: migration_20250918_184020.down,
+    name: '20250918_184020'
+  },
+];
