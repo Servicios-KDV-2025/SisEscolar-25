930968d1ccce859350f5d114e22d96f603e9b8ee
belterrwan <ramirezibarra95@hotmail.com>
2025-10-20 13:46:43 -0600

    Add custom sign-up and sign-in forms with OAuth support; update middleware for public route handling

--- apps/system/app/(auth)/[[...sign-in]]/page.tsx
+++ apps/system/app/(auth)/[[...sign-in]]/page.tsx
@@ -3,6 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 import { Authenticated, Unauthenticated } from "convex/react";
 import { useUser } from "@clerk/nextjs";
 import { UserSchools } from "components/UserSchools";
+import SignInForm from "components/CustomSignIn";
 
 export default function AuthPage() {
   const { user } = useUser();
@@ -17,7 +18,8 @@ export default function AuthPage() {
         </Authenticated>
         <Unauthenticated>
           <div className="flex flex-col items-center justify-center h-screen">
-              <SignIn />
+              {/* <SignIn /> */}
+              <SignInForm />
           </div>
         </Unauthenticated>
     </div>
