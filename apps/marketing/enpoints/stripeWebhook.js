0ad27f17edd29b9dabd81a2beb94efb53f40fca4
LuisGerardoRamosOchoa-WorkEdition <luis.ramos@g-tech.io>
2025-10-22 12:39:10 -0600

    Update dependencies in pnpm-lock.yaml and package.json to upgrade Stripe from version 18.4.0 to 19.1.0. Change Stripe API version in paymentSession.js, stripeCheckout.js, and stripeWebhook.js to 2025-09-30.clover for compatibility with the latest features.

--- apps/marketing/enpoints/stripeWebhook.js
+++ apps/marketing/enpoints/stripeWebhook.js
@@ -2,7 +2,7 @@ import Stripe from 'stripe'
 import sanityClient from '@sanity/client'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-09-30.clover',
 })
 
 // Configura tu cliente de Sanity
